a869d39f7b3b9d59be8644ef932b5e88e5f361d8
pcsmahendran <mahendran@purplecs.com>
2016-11-30 14:43:43 +0000

    init

--- PropertyManagement/Controllers/HomeController.cs
+++ PropertyManagement/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace PropertyManagement.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description 1.";
 
             return View();
         }
